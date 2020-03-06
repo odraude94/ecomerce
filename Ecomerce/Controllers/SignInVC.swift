bdfdc874ceaa77bab89cccbc0daaf75b38be8283
Eduardo Martinez <edirbn53@gmail.com>
2020-03-05 21:28:20 -0600

    console git

--- Ecomerce/Controllers/SignInVC.swift
+++ Ecomerce/Controllers/SignInVC.swift
@@ -14,6 +14,7 @@ class SignInVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .yellow
     }
     
 
